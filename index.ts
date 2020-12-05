834162f8c6dc214117e9b7cea9ee0fbd5f05fd38
Yogendra Singh <yog27ray@gmail.com>
2020-12-05 16:33:50 +0530

    update query formation

--- index.ts
+++ index.ts
@@ -5,10 +5,13 @@ import { MongoToParseError } from './src/error/mongo-to-parse-error';
 import { MongoToParseQueryBase, RequestQueryPayload, RequestCountPayload } from './src/transform/mongo-to-parse-query-base';
 
 class MongoToParseQuery extends MongoToParseQueryBase {
-  initialize(applicationId: string, serverURL: string, config: { masterKey?: string } = {}): void {
+  initialize(applicationId: string, serverURL: string, config: { masterKey?: string, disableSingleInstance?: boolean } = {}): void {
     Parse.initialize(applicationId, undefined, config.masterKey);
     Parse.serverURL = serverURL;
     this.setParse(Parse);
+    if (config.disableSingleInstance) {
+      Parse.Object.disableSingleInstance();
+    }
   }
 }
 
