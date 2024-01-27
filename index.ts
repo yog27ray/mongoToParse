dd4b5fa5cea53d35b7dfaee459599ba6b131cbd8
Yogendra Singh <yog27ray@gmail.com>
2024-01-27 08:10:23 +0530

    feat: Added fromJSON function.

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { MongoToParseError } from './src/error/mongo-to-parse-error';
 import { MongoToParseQueryBase, RequestCountPayload, RequestQueryPayload } from './src/transform/mongo-to-parse-query-base';
-import { ParseClassExtender, ParseObjectExtender } from './src/transform/parse-object-extender';
+import { ParseObjectExtender } from './src/transform/parse-object-extender';
 import { ParseRoleExtender } from './src/transform/parse-role-extender';
 
 let ParseLib;
@@ -50,7 +50,6 @@ export {
   MongoToParseError,
   MongoToParseQueryBase,
   ParseObjectExtender,
-  ParseClassExtender,
   ParseRoleExtender,
   RequestQueryPayload,
   RequestCountPayload,
