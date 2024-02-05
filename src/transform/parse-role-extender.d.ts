1d3a69ec7bc67cf6401c1135c68464b5f344fce3
Yogendra Singh <yog27ray@gmail.com>
2024-02-05 10:48:31 +0530

    v2.0.12

--- src/transform/parse-role-extender.d.ts
+++ src/transform/parse-role-extender.d.ts
@@ -2,7 +2,7 @@
 import { ParseObjectJSON } from './parse-type';
 export declare class ParseRoleExtender<T extends Parse.Attributes = Parse.Attributes> extends Parse.Role<T> {
     json?: ParseObjectJSON<T>;
-    constructor(name: string, acl: Parse.ACL);
+    constructor(name?: string, acl?: Parse.ACL);
     _toJSON(): ParseObjectJSON<T>;
     toJSON(): ParseObjectJSON<T>;
     save<K extends Extract<keyof T, string>>(attrs?: Pick<T, K> | T | null, options?: Parse.Object.SaveOptions): Promise<this>;
