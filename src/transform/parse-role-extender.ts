8dc12a61fc5d7295642b171ad7160e4e92700139
Yogendra Singh <yog27ray@gmail.com>
2024-02-05 10:47:39 +0530

    fix: type

--- src/transform/parse-role-extender.ts
+++ src/transform/parse-role-extender.ts
@@ -3,7 +3,7 @@ import { ParseObjectJSON } from './parse-type';
 export declare class ParseRoleExtender<T extends Parse.Attributes = Parse.Attributes> extends Parse.Role<T> {
   json?: ParseObjectJSON<T>;
 
-  constructor(name: string, acl: Parse.ACL);
+  constructor(name?: string, acl?: Parse.ACL);
 
   _toJSON(): ParseObjectJSON<T>;
 
