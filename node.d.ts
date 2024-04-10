a13b0f1bdaafb42f11d39da9271a8b63a336ee4f
Yogendra Singh <yog27ray@gmail.com>
2024-04-10 11:43:04 +0530

    v2.0.37

--- node.d.ts
+++ node.d.ts
@@ -1,5 +1,5 @@
 import { MongoToParseError } from './src/error/mongo-to-parse-error';
-import { MongoToParseQueryBase, ParseObjectAfterDeleteRequest, ParseObjectAfterFindRequest, ParseObjectAfterLoginRequest, ParseObjectAfterSaveRequest, ParseObjectBeforeDeleteRequest, ParseObjectBeforeFindRequest, ParseObjectBeforeLoginRequest, ParseObjectBeforeSaveRequest, RequestCountPayload, RequestQueryPayload } from './src/transform/mongo-to-parse-query-base';
+import { MongoToParseQueryBase, RequestCountPayload, RequestQueryPayload } from './src/transform/mongo-to-parse-query-base';
 import { ParseInstallationExtender } from './src/transform/parse-installation-extender';
 import { ParseObjectExtender } from './src/transform/parse-object-extender';
 import { ParseRoleExtender } from './src/transform/parse-role-extender';
@@ -12,4 +12,4 @@ declare class MongoToParseQuery extends MongoToParseQueryBase {
         disableSingleInstance?: boolean;
     }): void;
 }
-export { MongoToParseQuery, MongoToParseError, MongoToParseQueryBase, ParseObjectAfterDeleteRequest, ParseObjectAfterFindRequest, ParseObjectAfterLoginRequest, ParseObjectAfterSaveRequest, ParseObjectBeforeDeleteRequest, ParseObjectBeforeFindRequest, ParseObjectBeforeLoginRequest, ParseObjectBeforeSaveRequest, ParseObjectExtender, ParseRoleExtender, ParseSchemaExtender, ParseSessionExtender, ParseInstallationExtender, ParseUserExtender, RequestQueryPayload, RequestCountPayload, };
+export { MongoToParseQuery, MongoToParseError, MongoToParseQueryBase, ParseObjectExtender, ParseRoleExtender, ParseSchemaExtender, ParseSessionExtender, ParseInstallationExtender, ParseUserExtender, RequestQueryPayload, RequestCountPayload, };
