caf834b81dc982ea6acdf4dd8b21d45f1658540b
yog27ray <yog27ray@gmail.com>
2025-01-08 16:31:43 +0530

    feat: added release script

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const importPlugin = require("eslint-plugin-import");
 const { configs } = require("@eslint/js");
 const typescriptPlugin = require("eslint-plugin-typescript");
