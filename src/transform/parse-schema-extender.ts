6c0454f90f662b9bdf81d395e0e70c52ad450b17
faceapps <faceapps@heallo.ai>
2026-03-10 13:07:36 +0530

    package update

--- src/transform/parse-schema-extender.ts
+++ src/transform/parse-schema-extender.ts
@@ -1,4 +1,5 @@
+import ParseSchema from 'parse/types/ParseSchema';
 import { ParseObjectExtender } from './parse-object-extender';
 
-export declare class ParseSchemaExtender<T extends ParseObjectExtender> extends Parse.Schema<T> {
+export declare class ParseSchemaExtender<T extends ParseObjectExtender> extends ParseSchema<T> {
 }
